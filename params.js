3b43e0847f29f33c084b32dab16185978efe057a
duq <duque@intre.it>
2018-01-21 02:16:30 +0100

    Reports improved

--- params.js
+++ params.js
@@ -1,14 +1,14 @@
-const TRADE_INTERVAL_SECS=7;
-const AMOUNT_TO_TRADE = 0.01;//ETH
-const INVESTMENT=10;//EUR
-const ERROR_TOLERANCE=100;
-const PRODUCT_TYPE='ETH-EUR';
-const TRADE_SAMPLE_SIZE=7;
+const pollingInterval=7;
+const amountToTrade = 0.01;//ETH
+const investment=10;//EUR
+const errorTolerance=100;
+const productType='ETH-EUR';
+const tradeSampleSize=7;
 
-exports.TRADE_INTERVAL_SECS = TRADE_INTERVAL_SECS;
-exports.AMOUNT_TO_TRADE = AMOUNT_TO_TRADE;
-exports.INVESTMENT = INVESTMENT;
-exports.ERROR_TOLERANCE = ERROR_TOLERANCE;
-exports.PRODUCT_TYPE = PRODUCT_TYPE;
-exports.TRADE_SAMPLE_SIZE = TRADE_SAMPLE_SIZE;
+exports.pollingInterval = pollingInterval;
+exports.amountToTrade = amountToTrade;
+exports.investment = investment;
+exports.errorTolerance = errorTolerance;
+exports.productType = productType;
+exports.tradeSampleSize = tradeSampleSize;
 
--- trader.js
+++ trader.js
@@ -1,19 +1,24 @@
 const Gdax = require('gdax');
-const { TRADE_INTERVAL_SECS,
-        AMOUNT_TO_TRADE,
-        INVESTMENT,
-        ERROR_TOLERANCE,
-        PRODUCT_TYPE,
-        TRADE_SAMPLE_SIZE
-      } = require('./params');
+const { pollingInterval,
+  amountToTrade,
+  investment,
+  errorTolerance,
+  productType,
+  tradeSampleSize
+} = require('./params');
 
 const publicClient = new Gdax.PublicClient();
-const SELL = 'sell';
-const BUY = 'buy';
+const sell = 'sell';
+const buy = 'buy';
+const fromCurrency=productType.split('-')[0];
+const toCurrency=productType.split('-')[1];
 
-const TRADER_ID=TRADE_INTERVAL_SECS+"s|"+AMOUNT_TO_TRADE +"eth|"+INVESTMENT+"eur";
+const traderId=pollingInterval+"s|"
++amountToTrade +fromCurrency+"|"
++investment+toCurrency+"|"
++tradeSampleSize;
 
-let profits = INVESTMENT;
+let profits = investment;
 let iteration = 0;
 let bidsAverage=0;
 let lastBuyPrice=0;
@@ -23,32 +28,32 @@ let currentMarketPrice=0;
 let buys=0;
 let sells =0;
 let nIntervId;
-let lastAction = SELL;
+let lastAction = sell;
 let buyTimes=0;
 let sellTimes=0;
 let errors=0;
 
-let buy=(price) =>{
-  buyTimes++;
-  lastBuyPrice=price;
-  lastAction=BUY;
-  profits -= calculateTransactionAmount(price);
-  console.log("\n ------- Buying at: "+price +"(EUR) ------- Buy Times: "+buyTimes);
-};
-
-let sell=(price) =>{
+let doSell=(price) =>{
   sellTimes++;
   lastSellPrice=price;
-  lastAction=SELL;
+  lastAction=sell;
   profits += calculateTransactionAmount(price);
-  console.log("\n +++++++ Selling at: "+price +"(EUR) +++++++ Sell Times: "+sellTimes);
+  console.log("\n +++++++ Selling at: "+price +"("+toCurrency+") +++++++ Sell Times: "+sellTimes);
+};
+
+let doBuy=(price) =>{
+  buyTimes++;
+  lastBuyPrice=price;
+  lastAction=buy;
+  profits -= calculateTransactionAmount(price);
+  console.log("\n ------- Buying at: "+price+"("+toCurrency+") ------- Buy Times: "+buyTimes);
 };
 
 let getAverage = (items) =>{
 	var sumItems = items.reduce((accumulator, item) => {
                 //"item": [ price, size, num-orders ] 
-    return accumulator + parseInt(item[0]);
-  }, 0);
+                return accumulator + parseInt(item[0]);
+              }, 0);
 
 	return sumItems/items.length;
 };
@@ -56,63 +61,61 @@ let getAverage = (items) =>{
 let getPreciseAverage = (items) =>{
   var size = items.reduce((accumulator, item) => {
                 //"item": [ price, size, num-orders ] 
-    return accumulator + parseInt(item[2]);
-  }, 0);
+                return accumulator + parseInt(item[2]);
+              }, 0);
 
   var sumItems = items.reduce((accumulator, item) => {
                 //"item": [ price, size, num-orders ] 
-    return accumulator + parseInt(item[0]);
-  }, 0);
+                return accumulator + parseInt(item[0]);
+              }, 0);
 
   return sumItems/size;
 };
 
-let calculateTransactionAmount = (price) => {return price * AMOUNT_TO_TRADE};
+let calculateTransactionAmount = (price) => {return price * amountToTrade};
 
 let askForInfo = ()=>{
-  publicClient.getProductOrderBook(PRODUCT_TYPE, { level: 2 })
+
+  publicClient.getProductOrderBook(productType, { level: 2 })
   .then(data => {
-    //console.log("...............// Get the order book at the default level of detail.");
     //console.log(data);
     bidsAverage = getAverage(data.bids);
-    console.log("Bids Average: " +bidsAverage +'');
-
     asksAverage = getAverage(data.asks);
+    console.log("Bids Average: " +bidsAverage +'');
     console.log("Asks Average: " +asksAverage +'');
   })
   .catch(error => {
-    errors++;
     //TODO handle error
+    errors++;
     console.log(error);
   });
 
-  publicClient.getProductTicker(PRODUCT_TYPE)
+
+  publicClient.getProductTicker(productType)
   .then(data => {
-    //console.log("// publicClient.getProductTicker(PRODUCT_TYPE");
     //console.log(data);
     currentMarketPrice = data.price;
   })
   .catch(error => {
-    errors++;
     //TODO handle error
+    errors++;
     console.log(error);
   });
 
-  publicClient.getProductTrades(PRODUCT_TYPE, {limit:TRADE_SAMPLE_SIZE})
+
+  publicClient.getProductTrades(productType, {limit:tradeSampleSize})
   .then(data => {
-    //console.log("// To make paginated requests, include page parameters");
     //console.log(data);
-    buys = data.filter(data => data.side === BUY).length;
+    buys = data.filter(data => data.side === buy).length;
+    sells = data.filter(data => data.side === sell).length;
     console.log('Buyers: ' + buys);
-    sells = data.filter(data => data.side === SELL).length;
     console.log('Sellers: ' + sells);
   })
   .catch(error => {
-    errors++;
     //TODO handle error
+    errors++;
     console.log(error);
   });  
-
   //gdaxTime();
 };
 
@@ -123,54 +126,53 @@ let gdaxTime = () =>{
     console.log(data);
   })
   .catch(error => {
-    errors++;
     //TODO handle error
+    errors++;
     console.log(error);
   });
 };
 
-let makeChoice = () =>{
+let makeAChoice = () =>{
   if(iteration == 1){
-     buy(bidsAverage); // I assuming first action will be to Buy
+     doBuy(bidsAverage); // I'm assuming first action will be to Buy
    }
-  if(sells > buys && lastAction!==BUY && lastSellPrice >= bidsAverage){
-    buy(bidsAverage);
+   if(sells > buys && lastAction!==buy && lastSellPrice >= bidsAverage){
+    doBuy(bidsAverage);
   }
-  if(buys > sells && lastAction!==SELL && asksAverage > lastBuyPrice){
-    sell(asksAverage);
+  if(buys > sells && lastAction!==sell && asksAverage > lastBuyPrice){
+    doSell(asksAverage);
   }
 }
 
 let doTrade=() => {
   iteration++;
 
-  makeChoice();
+  makeAChoice();
   askForInfo(); 
 
   printReport();
 };
 
 let printReport= ()=>{
-  console.log("\n" + new Date() + " - " + "Iteration #" + iteration);
-  console.log("Trader_id: "+TRADER_ID+"  >>> Profits: " +  profits+" (EUR)");
-  console.log("Buy Times: " + buyTimes);
-  console.log("Sell Times: " + sellTimes);
-  console.log("Current Price: " + currentMarketPrice);
-  console.log("Last Buy Price: " +  lastBuyPrice);
-  console.log("Last Sell Price: " +  lastSellPrice);
-  console.log("Errors: " + errors);
-  console.log("");
+  console.log("\n--------------------------------------------------------------");
+  console.log("  "+new Date()+"   "+"Iteration #"+iteration);
+  console.log("  Trader# "+traderId+"                Errors: "+ errors);    
+  console.log("  Last Buy     :  "+lastBuyPrice+"("+toCurrency+")"+"               Buy Times: "+ buyTimes); 
+  console.log("  Last Sell    :  "+lastSellPrice+"("+toCurrency+")"+"               Sell Times: "+ sellTimes);
+  console.log("  Current Price:  "+ currentMarketPrice+"("+toCurrency+")");
+  console.log("  Profits      :  "+profits+"("+toCurrency+")");
+  console.log("-------------------------------------------------------------\n");
 
   if(profits <= 0){
     console.log("\n   !!!!!!!!  SORRY MAN, YOU'RE BANKRUPT.  !!!!!!!!\n");
     clearInterval(nIntervId);
   }
 
-  if(errors > ERROR_TOLERANCE){
-    console.log("\n   !!!!!!!!  ERROR_TOLERANCE OUT OF LIMIT.  !!!!!!!!\n");
+  if(errors > errorTolerance){
+    console.log("\n   !!!!!!!!  ERROR TOLERANCE OUT OF LIMIT.  !!!!!!!!\n");
     clearInterval(nIntervId); 
   }
 };
 
 askForInfo();
-nIntervId = setInterval(doTrade, TRADE_INTERVAL_SECS*1000);
+nIntervId = setInterval(doTrade, pollingInterval*1000);
