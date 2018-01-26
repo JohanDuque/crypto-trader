1fd4f2e827e399e19f90038d02317d55663c9e9f
duq <duque@intre.it>
2018-01-26 21:43:21 +0100

    Strategy V4

--- StrategyFactory.js
+++ StrategyFactory.js
@@ -3,11 +3,13 @@ const config = require('./Configuration');
 const Strategy_V1 = require('./strategies/Strategy_V1');
 const Strategy_V2 = require('./strategies/Strategy_V2');
 const Strategy_V3 = require('./strategies/Strategy_V3');
+const Strategy_V4 = require('./strategies/Strategy_V4');
 
 const strategies = new Map();
 strategies.set('V1', Strategy_V1);
 strategies.set('V2', Strategy_V2);
 strategies.set('V3', Strategy_V3);
+strategies.set('V4', Strategy_V4);
 
 module.exports = class StrategyFactory {
 	static getStrategy(){
---
+++ strategies/Strategy_v4.js
@@ -0,0 +1,29 @@
+const gb = require('../GlobalVariables');
+const trader = require('../Trader');
+const conf = require('../Configuration');
+const Logger = require('../Logger');
+
+module.exports = class Strategy_V4 {
+    static apply() {
+        let betterAverage;
+
+        if (gb.lastSellers < gb.lastBuyers && gb.sellOrders === 0) {
+            gb.lastSellPrice = gb.currentMarketPrice; //This is only to give a starting point
+        }else{
+
+        if (gb.lastAction !== conf.BUY && gb.lastSellPrice >= gb.currentMarketPrice && gb.lastOrderWasFilled) {
+            betterAverage = (gb.bidsAverage + gb.currentMarketPrice) / 2;
+            if (conf.logLvl >= 2) Logger.log("Improved Average: " + betterAverage);
+
+            trader.doBuy(betterAverage);
+        }
+
+        if (gb.lastAction !== conf.SELL && gb.lastBuyPrice < gb.currentMarketPrice && gb.lastOrderWasFilled) {
+            betterAverage = (gb.asksAverage + gb.currentMarketPrice) / 2;
+            if (conf.logLvl >= 2) Logger.log("Improved Average: " + betterAverage);
+
+            trader.doSell(betterAverage);
+        }
+    }
+    };
+}
