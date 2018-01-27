3f113b0228cd7a50c1e30c158bd65e8bf9d63458
duq <duque@intre.it>
2018-01-27 18:13:17 +0100

    Strategy_V5

---
+++ strategies/Strategy_V5.js
@@ -0,0 +1,50 @@
+const gb = require('../GlobalVariables');
+const trader = require('../Trader');
+const conf = require('../Configuration');
+const Logger = require('../Logger');
+
+let isStarting = gb.sellOrders === gb.buyOrders &&
+    gb.buyOrders === gb.fills &&
+    gb.fills === gb.lastSellPrice &&
+    gb.lastSellPrice === gb.lastBuyPrice &&
+    gb.lastBuyPrice === 0;
+
+module.exports = class Strategy_V5 {
+    static apply() {
+        let betterAverage;
+
+        if (gb.lastOrderWasFilled) {
+            //While market is constantly going UP...
+            if (gb.currentBuyers > gb.currentSellers + conf.tradeHistorySize / 4) {
+                if (gb.lastAction !== conf.BUY) {
+                    trader.doBuy(gb.currentMarketPrice);
+                    console.log("Market is constantly going UP, I'm buying!");
+                }
+                return;
+            }
+
+            //While market is constantly going DOWN...
+            if (gb.currentSellers > gb.currentBuyers + conf.tradeHistorySize / 4) {
+                if (gb.lastAction !== conf.BUY) {
+                    console.log("Market is going DOWN fast, I will wait to SELL");
+                    return;
+                }
+            }
+
+            if (gb.lastAction !== conf.BUY && gb.lastSellPrice >= gb.currentMarketPrice) {
+                improvedAverage = (gb.bidsAverage + gb.currentMarketPrice) / 2;
+                if (conf.logLvl >= 2) Logger.log("Improved Average: " + improvedAverage);
+
+                trader.doBuy(improvedAverage);
+                return;
+            }
+
+            if (gb.lastAction !== conf.SELL && gb.lastBuyPrice < gb.currentMarketPrice) {
+                improvedAverage = (gb.asksAverage + gb.currentMarketPrice) / 2;
+                if (conf.logLvl >= 2) Logger.log("Improved Average: " + improvedAverage);
+                trader.doSell(improvedAverage);
+                return;
+            }
+        }
+    }
+};
