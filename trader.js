5f18639f86a49768128ffd868a33a8a8a41668ac
duq <duque@intre.it>
2018-01-27 13:49:31 +0100

    Trader logger improved

--- trader.js
+++ trader.js
@@ -11,6 +11,7 @@ class Trader {
         gb.lastOrderWasFilled = false;
 
         if (conf.logLvl >= 1) Logger.log("\n+++++ Placing Sell Order at: " + price + "(" + conf.toCurrency + ") +++++ Sell Orders: " + gb.sellOrders);
+        if (conf.logLvl >= 0) Logger.printReport();
     }
 
     doBuy(price) {
@@ -21,6 +22,7 @@ class Trader {
         gb.lastOrderWasFilled = false;
 
         if (conf.logLvl >= 1) Logger.log("\n----- Placing Buy Order at: " + price + "(" + conf.toCurrency + ") ----- Buy Orders: " + gb.buyOrders);
+        if (conf.logLvl >= 0) Logger.printReport();
     }
 
     calculateTransactionAmount(price) {
