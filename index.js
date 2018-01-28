2ee9f35c639f37a3517b4b5528ef781890ff4061
duq <duque@intre.it>
2018-01-28 10:47:55 +0100

    Order fill erroe Delta temporary remove

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ let checkFills = () => {
 
     if (!gb.lastOrderWasFilled) {
         if (gb.lastAction === conf.BUY) {
-            wasItFilled = gb.tradeHistory.find((data) => {
-                return (gb.lastBuyPrice - data.price) >= 0 || Math.abs(gb.lastBuyPrice - data.price) <= conf.orderFillError
-            });
+            wasItFilled = gb.tradeHistory.find((trade) => {
+                //return (gb.lastBuyPrice - trade.price) > 0 || Math.abs(gb.lastBuyPrice - trade.price) <= conf.orderFillError
+                return (gb.lastBuyPrice - trade.price) > 0 });
 
         } else { //gb.lastAction === sell
-            wasItFilled = gb.tradeHistory.find((data) => {
-                return (gb.lastSellPrice - data.price) <= 0 || Math.abs(gb.lastSellPrice - data.price) <= conf.orderFillError
-            });
+            wasItFilled = gb.tradeHistory.find((trade) => {
+                //return (gb.lastSellPrice - trade.price) <= 0 || Math.abs(gb.lastSellPrice - data.price) <= conf.orderFillError
+                return (gb.lastSellPrice - trade.price) < 0 });
         }
 
         gb.lastOrderWasFilled = wasItFilled !== undefined;
@@ -86,10 +86,10 @@ let getMeanTradeFrequency = () => {
 let doTrade = () => {
     gb.iteration++;
 
-    if (conf.logLvl >= 2) Logger.log("Asking for info at: " + new Date());
+    if (conf.logLvl >= 2) Logger.log("\nAsking for info at: " + new Date());
 
     askForInfo().then(() => {
-        if (conf.logLvl >= 2) Logger.log("Info received at:   " + new Date());
+        if (conf.logLvl >= 2) Logger.log("Info received at:   " + new Date() +"\n");
         checkFills();
         applyStrategy();
 
