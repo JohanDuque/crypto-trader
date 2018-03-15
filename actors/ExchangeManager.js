503055e0634658587ef19e7219a64398225fb5ca
duq <duque@intre.it>
2018-03-15 09:31:07 +0100

    ExchangeManager refactored

--- actors/ExchangeManager.js
+++ actors/ExchangeManager.js
@@ -24,21 +24,20 @@ class ExchangeManager {
         throw new Error('You have to implement this method!');
     }
 
+    getOrderBook() {
+        throw new Error('You have to implement this method!');
+    }
+
+    getTradeHistory() {
+        throw new Error('You have to implement this method!');
+    }
+    
     getAverage(items) {
         let sumItems = items.reduce((accumulator, item) => {
             //"item": [ price, size, num-orders ]
             return accumulator + parseInt(item[0]);
         }, 0);
         return sumItems / items.length;
-    }
-
-    getOrderBook() {
-        throw new Error('You have to implement this method!');
-    }
-
-
-    getTradeHistory() {
-        throw new Error('You have to implement this method!');
     }
 }
 
