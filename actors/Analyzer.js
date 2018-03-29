5839e0f0c24168a628e799d3cc4a3ceb6c35f9eb
duq <duque@intre.it>
2018-03-29 09:27:52 +0200

    Upper and Lower factor methods added to Analyzer

--- actors/Analyzer.js
+++ actors/Analyzer.js
@@ -1,19 +1,17 @@
 const gb = require('../commons/GlobalVariables');
 
 class Analyzer {
-    isBuySpeedIncreasing() { return gb.currentBuySpeed > gb.lastBuySpeed }
-    isSellSpeedIncreasing() { return gb.currentSellSpeed > gb.lastSellSpeed }
+    isBuySpeedIncreasing(){ return gb.currentBuySpeed > gb.lastBuySpeed;}
+    isSellSpeedIncreasing(){ return gb.currentSellSpeed > gb.lastSellSpeed;}
 
-    areBuyersTwiceSellers() { return gb.currentBuyers / gb.currentSellers > 2; }
-    areSellersTwiceBuyers() { return gb.currentSellers / gb.currentBuyers > 2; }
+    areBuyersTwiceSellers(){ return gb.currentSellers / gb.currentBuyers > 2;}
+    areSellersTwiceBuyers(){ return gb.currentBuyers / gb.currentSellers > 2;}
 
-    isMarketGoingFromDownToUp() {
-        return this.isBuySpeedIncreasing() && (gb.lastSellSpeed > 0.1) && (gb.currentSellSpeed < 0.1);
-    }
+    isMarketGoingFromDownToUp(){ return  this.isBuySpeedIncreasing() && (gb.lastSellSpeed > 0.1) && (gb.currentSellSpeed < 0.1);}
+    isMarketGoingFromUpToDown(){ return  this.isSellSpeedIncreasing() && (gb.lastBuySpeed > 0.1) && (gb.currentBuySpeed < 0.5);}
 
-    isMarketGoingFromUpToDown() {
-        return this.isSellSpeedIncreasing() && (gb.lastBuySpeed > 0.1) && (gb.currentBuySpeed < 0.5);
-    }
+    isLastBuyLowerByFactor(factor){ return gb.currentMarketPrice < gb.lastBuyPrice - (gb.lastBuyPrice * factor);}
+    isLastBuyUpperByFactor(factor){ return gb.currentMarketPrice > gb.lastBuyPrice + (gb.lastBuyPrice * factor);}
 }
 
 
